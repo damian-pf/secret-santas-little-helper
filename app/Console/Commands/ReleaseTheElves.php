237284e588de7d66cf859c7dfb66b37a647cda70
Damian Redpath <damian@pixelfusion.co.nz>
2017-11-09 22:55:59 +1300

    Tidy up

--- app/Console/Commands/ReleaseTheElves.php
+++ app/Console/Commands/ReleaseTheElves.php
@@ -41,16 +41,6 @@ class ReleaseTheElves extends Command
      */
     public function handle()
     {
-        // get all people
-        // sort by longest list of not allowed people
-        // while dont have a list call generate function
-            // generate function will
-            // have a empty list
-            // for each person
-            // diff the remaining people with the people they may not match
-            // if the remaining list is empty return false (this should cause the loop to call function again
-            // if not empty pick a random and add the key/value pair to the list
-
         $people = collect(config('people'));
         $sortedPeople = $people->sort(function ($personA, $personB) {
             return count($personB['blocked']) <=> count($personA['blocked']);
@@ -59,8 +49,6 @@ class ReleaseTheElves extends Command
         do {
             $this->matchPeople($sortedPeople);
         } while (empty($this->matchedPeople) === true);
-
-        dd($this->matchedPeople);
 
         // go through matches and send sms or email
         foreach ($this->matchedPeople as $sender => $receiver) {
